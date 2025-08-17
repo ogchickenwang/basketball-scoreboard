32353c0b98b97a904823d94421c78b0752c71a96
ogchickenwang <chick3nwang@gmail.com>
2025-08-16 20:18:54 -0700

    Commit initial files

---
+++ index.js
@@ -0,0 +1,34 @@
+let homeScoreEl = document.getElementById("home-score-el")
+let guestScoreEl = document.getElementById("guest-score-el")
+let homeScore = 0
+let guestScore = 0
+
+function add1Home() {
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+}
+
+function add2Home() {
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+}
+
+function add3Home() {
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+}
+
+function add1Guest() {
+    guestScore += 1
+    guestScoreEl.textContent = guestScore
+}
+
+function add2Guest() {
+    guestScore += 2
+    guestScoreEl.textContent = guestScore
+}
+
+function add3Guest() {
+    guestScore += 3
+    guestScoreEl.textContent = guestScore
+}
